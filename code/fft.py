2ca523fc3741e4720e53266ee0ad9d94b80299f2
fedor <chayka.fedor@gmail.com>
2025-01-18 20:21:41 -0800

    rewrote fft.py to use functions

--- code/fft.py
+++ code/fft.py
@@ -1,67 +1,73 @@
+
 import scipy
 import numpy as np
-import matplotlib.pyplot as plt
 import operator
-
-# CONFIG ##############################################################
-FREQ_CUTOFF_LOW = 40 # Hz
-FREQ_CUTOFF_HIGH = 4000 # Hz
-MIN_PEAK_DIST = 10 # Hz
-TOP_PEAK_NUM = 50
-
-DO_PLOTTING = False
 
 # LOAD SOUND FILE #####################################################
 
-SAMPLERATE, data = scipy.io.wavfile.read("../../FFT_data/test_music/397_meantone.wav")
-#SAMPLERATE, data = scipy.io.wavfile.read("./test_400_440_5sec.wav")
+def load_file(dir):
+    SAMPLERATE, data = scipy.io.wavfile.read(dir)
 
-if data.ndim > 1:
-    print("data is not mono")
-    exit()
+    if data.ndim > 1:
+        print("data is not mono")
+        exit()
 
-N_SAMPLES = len(data)
+    N_SAMPLES = len(data)
 
-print("samplerate = ", SAMPLERATE)
-print("n_samples = ", N_SAMPLES)
-print("duration = ", N_SAMPLES / SAMPLERATE)
-print("max freq = ", SAMPLERATE / 2)
-print("bin size = ", SAMPLERATE / N_SAMPLES)
+    print("samplerate = ", SAMPLERATE)
+    print("n_samples = ", N_SAMPLES)
+    print("duration = ", N_SAMPLES / SAMPLERATE)
+    print("max freq = ", SAMPLERATE / 2)
+    print("bin size = ", SAMPLERATE / N_SAMPLES)
+
+    return data, SAMPLERATE
 
 # RUN FOURIER TRANSFORM ###############################################
 
-complex_fourier = scipy.fft.fft(data)
-abs_fourier = np.abs(complex_fourier[:N_SAMPLES // 2])
+def run_fourier(data, sample_rate, smoothing=True, freq_cutoff=False, frq_low=0, frq_high=1):
+    complex_fourier = scipy.fft.fft(data)
+    abs_fourier = np.abs(complex_fourier[:len(data) // 2])
 
-freq = scipy.fft.fftfreq(N_SAMPLES, d=1/SAMPLERATE)
-freq = freq[0:N_SAMPLES // 2]
+    freq = scipy.fft.fftfreq(len(data), d=1/sample_rate)
+    freq = freq[0:len(data) // 2]
 
-smooth_fourier = scipy.signal.savgol_filter(abs_fourier, 100, 3)
+    if freq_cutoff:
+        freq = freq[round(frq_low/(sample_rate/len(data))):round(frq_high/(sample_rate/len(data)))]
+        abs_fourier = abs_fourier[round(frq_low/(sample_rate/len(data))):round(frq_high/(sample_rate/len(data)))]
+    
+    if smoothing:
+        smooth_fourier = scipy.signal.savgol_filter(abs_fourier, 100, 3)
+        return freq, smooth_fourier
+    else:
+        return freq, abs_fourier
+    
 
-# frequency cutoffs:
-abs_fourier = abs_fourier[round(FREQ_CUTOFF_LOW/(SAMPLERATE/N_SAMPLES)):round(FREQ_CUTOFF_HIGH/(SAMPLERATE/N_SAMPLES))]
-smooth_fourier = smooth_fourier[round(FREQ_CUTOFF_LOW/(SAMPLERATE/N_SAMPLES)):round(FREQ_CUTOFF_HIGH/(SAMPLERATE/N_SAMPLES))]
-freq = freq[round(FREQ_CUTOFF_LOW/(SAMPLERATE/N_SAMPLES)):round(FREQ_CUTOFF_HIGH/(SAMPLERATE/N_SAMPLES))]
+def find_top_peaks(data, sample_rate, peak_num, peak_dist): # peak dist in Hz
+    # find peaks:
+    peaks,_ = scipy.signal.find_peaks(data, distance=round(peak_dist/(sample_rate / len(data))))
+    # compute peak prominences:
+    peaks_prom = scipy.signal.peak_prominences(data, peaks)[0]
+    # sort peaks by prominence, write most prominent ones to top_peaks
+    sorted_peaks,_ = zip(*sorted(zip(peaks, peaks_prom), key=operator.itemgetter(1), reverse=True))
+    top_peaks = sorted_peaks[1:peak_num] # *indices* of top peaks
 
-# find peaks:
-peaks,_ = scipy.signal.find_peaks(smooth_fourier, distance=round(MIN_PEAK_DIST/(SAMPLERATE / N_SAMPLES)))
-# compute peak prominences:
-peaks_prom = scipy.signal.peak_prominences(smooth_fourier, peaks)[0]
-# sort peaks by prominence, write TOP_PEAK_NUM most prominent ones to top_peaks
-sorted_peaks,_ = zip(*sorted(zip(peaks, peaks_prom), key=operator.itemgetter(1), reverse=True))
-top_peaks = sorted_peaks[1:TOP_PEAK_NUM] # *indices* of top peaks
+    top_peaks_freq = [round(i*(sample_rate / len(data)), 2) for i in top_peaks]
 
-top_peaks_freq = [round(i*(SAMPLERATE / N_SAMPLES), 2) for i in top_peaks]
+    return top_peaks, top_peaks_freq
 
-# PLOTTING ############################################################
 
-if DO_PLOTTING:
-    plt.plot(freq, abs_fourier)
-    plt.plot(freq, smooth_fourier)
-    for xc in [freq[i] for i in top_peaks]:
-        plt.axvline(x=xc, color='red')
-    plt.show()
 
-# DATA OUTPUT #########################################################
+data, sample_rate = load_file("../../FFT_data/test_music/397_meantone.wav", )
+freq, fourier = run_fourier(data, sample_rate, smoothing=False, freq_cutoff=True, frq_low=40, frq_high=4000)
+_, smooth_fourier = run_fourier(data, sample_rate, smoothing=True, freq_cutoff=True, frq_low=40, frq_high=4000)
 
-np.savetxt('top_peaks.csv', (top_peaks, top_peaks_freq), delimiter=',',fmt='%.2f')
+peaks, peaks_freq = find_top_peaks(smooth_fourier, sample_rate, peak_num=50, peak_dist=10)
+np.savetxt('top_peaks.csv', (peaks, peaks_freq), delimiter=',',fmt='%.2f')
+
+import matplotlib.pyplot as plt
+
+plt.plot(freq, fourier)
+plt.plot(freq, smooth_fourier)
+for xc in [freq[i] for i in peaks]:
+    plt.axvline(x=xc, color='red')
+plt.show()
